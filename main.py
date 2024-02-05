ca85ff4e07facd30b7fd881a915b697ace7301b0
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-05 12:10:18 +0100

    cleaned the code

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 # <<<-- S.S.S GOOD LUCK -->>>
-# multiple assignments at once
-# Age = Number = Point = 20
-# print(Age)
-# print(Number)
-# print(Point)
 
-first_name = "Wadie"
-last_name = "Ben"
-fullName = first_name + last_name
 
-print(fullName)
