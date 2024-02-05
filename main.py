eda4003d676a4772cd644796b60367fddaf8468e
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-05 12:13:48 +0100

    added book name I'm using + intro to numbers.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# <<<-- S.S.S GOOD LUCK -->>>
+# <<<-- S.S.S GOOD LUCK
+# Book started with -> "Python Programming Illustrated by William Sullivan"
+# -->>>
 
+# Numbers, there are 4 types 
+# int, float, long, and complex
 
+int_num = 10
+float_num = 141.6
+long_num = -0.5977485613454646
+complex_num = -.785+7J
