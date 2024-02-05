e46940a4ca9480c3268ef1b098afd3436d18b8c7
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-05 11:57:11 +0100

    Multiple Assignment

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
 # <<<-- S.S.S GOOD LUCK -->>>
-    
-# -------- Club age checking-------
-legalAge = 18
-clientAge = 23
-
-if legalAge > clientAge:
-    print("You are not allowed to be here!")
-else:
-    print("You are welcome here!")
-# -------- Club age checking end-------
-
-# capturing user input, terminal will have the input
-# text = input("Please enter your name ")
-# print(text, "is 23 years old")
-
-
-# Variables in a nutshell
-name = "Wadie"  #string variable.
-age = 23        #integer variable.
-score = 141.6   #a floating type variable.
-tacticalGuy = True #a boolean Variable, (notice True starts with a capital T)
-
-print(type(tacticalGuy)) #that's how we check the tyoe of a variable
+# multiple assignments at once
+Age = Number = Point = 20
+print(Age + Number + Point)
