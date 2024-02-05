77f44aefcecae9d234a7686db991cdbb542e976c
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-05 12:09:24 +0100

    strings concatenation

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # <<<-- S.S.S GOOD LUCK -->>>
 # multiple assignments at once
-Age = Number = Point = 20
-print(Age + Number + Point)
+# Age = Number = Point = 20
+# print(Age)
+# print(Number)
+# print(Point)
+
+first_name = "Wadie"
+last_name = "Ben"
+fullName = first_name + last_name
+
+print(fullName)
