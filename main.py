ab58b31516a907fd75bf3fcd6db87ceff56e2696
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-05 12:41:43 +0100

    lists vs tuples

--- main.py
+++ main.py
@@ -2,7 +2,18 @@
 # Book started with -> "Python Programming Illustrated by William Sullivan"
 # -->>>
 
-# lists aka arrays
-realFriends = ["Bike", "Screwdriver", "Core Prime", "Shtora-LightT90", "QuechuaBag", "Oshee Water Bottle"]
-print(len(realFriends)) #len means the "length" or total
+# list
+cars = ["Audi", "Mercededs", "Jaguar", "BMW"] #index like JS, start with 0.
+cars[2] = "GMC" #modifying an list item using reassign trick.
+print(cars[1]) #print the third, yes the third and not the second item, because we start with 0 just like JS.
+cars.append("Tesla") #push an item into the list.
+print(cars)
+
+# Tuples - They use () instead of []
+# They are immutables, meaning that thei value cannot change.
+clubTeams = ("FC Barcelona", "Real Madrid", "Chelsea")
+# this code won't work => clubTeams(2) = "Arsenal"
+# because Tuples are immuatbles just like JS const.
+print(clubTeams)
+# done
 
