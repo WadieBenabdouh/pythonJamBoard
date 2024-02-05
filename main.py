469b8f52fa82f18dea44f5958fb3fb31eabae284
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-05 12:58:31 +0100

    added my exaxmple for dictionaries

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ cars = {"Name":"Audi", "Model":2008, "Color":"Black"}
 #.keys/.values are methods
 print(cars.keys()) #Name, Model, Color
 print(cars.values()) #Audi, 2008, Black
+
+
+trueFriend = {"Name": "Bike", "Model":2010, "Engine":"S.S.S Core Concept"}
+print(trueFriend.keys(), trueFriend.values())
