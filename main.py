2ffd070c389d39c70299e3d036fc1cc8df43f4ff
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-10 01:03:01 +0100

    printed a legendary message

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 # <<<-- S.S.S GOOD LUCK
 # Book started with -> "Python Programming Illustrated by William Sullivan"
 # -->>>
-
-# OPERATORS
-# Python operators can be divided into following five categories:
-# 1- Arithmetic
-# 2- Logical
-# 3- Comparison
-# 4- Assignement
-# 5- Membership operators
-
-# -- 2 Logical operators
-# 
-
-
+trueFriends = ["MyBike", "CorePrime", "TacticalNokia", "Scredriver", "Red and Black Shoes", "Rear Light Shtora T90A"]
+print(trueFriends[2], "and", trueFriends[0], "are considered a solid tank.")
