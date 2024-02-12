444107cf26b350a3cebb1debfb65bd5083463df8
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-12 11:11:16 +0100

    learned about -> > P E M D A S <<-

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 # <<<-- S.S.S GOOD LUCK
 # Book started with -> "Python Programming Illustrated by William Sullivan"
 # -->>>
-T90A_Specs = {
-    "Speed" : "70km/h and -4km/h",
-    "Protection" : "ERA and slopped armor",
-    "ExtraDefenseLayers" : "Shtora and Smoke launchers",
-    "Scope" : "NVD and Rangefinder"
+
+# PEMDAS
+Pemdas = {
+    "P" : "stands for parantheses operations",
+    "E" : "stands for exponents (power)",
+    "M" : "stands for multiplcation",
+    "D" : "stands for division",
+    "A" : "stands for division",
+    "S" : "stands for substaction"
 }
-
-
-
-
-
-
-
-
-
