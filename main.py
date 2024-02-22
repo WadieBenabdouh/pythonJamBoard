6e002030ebbd623358092b5d60b0861aa9bdf24e
Wadie Benabdouh <wadie.trade@gmail.com>
2024-02-23 00:32:45 +0100

    great start w/ J.Nb

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-user_input = input()
-print(type(user_input))
-converted_input = int(user_input)
-print(type(converted_input))
+legal_age = 18
+user_age = input()
+# converting the input from str to integer.
+converted_age = int(user_age)
+
+# verification system using conditionals.
+if legal_age < converted_age:
+    print("You are allowed to access the club")
     
+elif legal_age == converted_age:
+    print("Perfect age amigo")
+    
+else :
+    print("You are not allowed for entry")
